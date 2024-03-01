17bab30464c58ebde0d42a9cdcb9ac7a68179877
Francesco <stellar.database@gmail.com>
2024-03-01 14:34:28 +0100

    Remove unused using statements in OctreeNode.cs

--- Assets/SimulationScript/OctreeNode.cs
+++ Assets/SimulationScript/OctreeNode.cs
@@ -1,8 +1,4 @@
 using System.Collections.Generic;
-using System.Numerics;
-using Unity.Collections;
-using Unity.Jobs;
-using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 public class OctreeNode
